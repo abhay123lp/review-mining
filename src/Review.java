cb41f564db8d87f0041ddd7887cf65b0c980f734
kaiserasif <kaiserasif@gmail.com>
2012-04-08 18:40:14 -0500

    DAO

--- src/Review.java
+++ src/Review.java
@@ -1,4 +1,16 @@
+import java.util.Date;
+
 
 public class Review {
-
+	int id;
+	int reviewerId;
+	int storeId;
+	Date date;
+	int rating;
+	String text;
+	Date dateModified;
+	int siteId;
+	int duplicateCount;
+	boolean isModified;
+	boolean topReviewer;
 }
