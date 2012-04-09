169b2e0a94d41f070a1338789203a806a55e596f
kaiserasif <kaiserasif@gmail.com>
2012-04-09 12:34:43 -0500

    start on wordlist

--- src/SO.java
+++ src/SO.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 
 public class SO {
 	Tagging posTagger = new Tagging();
+	static HashMap<String, Integer> opinionLexicon = loadLexicons();
 	
+	static HashMap<String, Integer> loadLexicons() {
+		HashMap<String, Integer> lx = new HashMap<String, Integer>();
+		
+		return lx;
+	}
 	/**
 	 * Gets the two-words phrases of Adj-N, Adv-Adj, Adj-Adj, N-Adj or Adv-V form using POS and search
 	 * @param text to be analyzed
@@ -56,5 +64,19 @@ public class SO {
 		}
 		
 		return phrases;
+	}
+	
+	/**
+	 * Returns a phrase if it's world are majority in +ve or -ve side
+	 * Need to formulate better algorithm, e.g. "extremely bad" vs "extremely good"
+	 * @param s is the string
+	 * @return -1, 1 for majority class, 0 for neutral or not-found.  
+	 */
+	public int getPolarityOfPhrase(String s) {
+		int k = 0;
+		for (String st: s.split(" ")) {
+			System.out.println(st.replaceAll("[^\\w]*", ""));
+		}
+		return 0;
 	}
 }
