1f7abfbb1ec9e3f5ee5a90aaf438751dceb47f83
shaoxinjiang <shaoxinjiang@gmailcom>
2012-04-08 21:58:00 -0500

    Added WebCrawler Lib and Tagging

--- src/Tagging.java
+++ src/Tagging.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,20 +8,43 @@ import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.ling.TaggedWord;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
-
+// get more information on 
+// http://www.computing.dcu.ie/~acahill/tagset.html
+// http://nlp.stanford.edu/software/tagger.shtml
 class Tagging {
-
-  public void test() throws Exception {
-	String modelFile="config/models/wsj-0-18-bidirectional-distsim.tagger";
-    MaxentTagger tagger = new MaxentTagger(modelFile);
-    @SuppressWarnings("unchecked")
-    String testFile = "bin/config/sampleinput.txt";
-    List<List<HasWord>> sentences = tagger.tokenizeText(new BufferedReader(new FileReader(testFile)));
-    for (List<HasWord> sentence : sentences) {
-      ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
-      System.out.println(Sentence.listToString(tSentence, false));
-    }
-  }
+	public String modelFile;
+	
+	Tagging(){
+		modelFile="config/models/wsj-0-18-bidirectional-distsim.tagger";
+	}
+	
+	public void test() throws Exception {
+	
+		MaxentTagger tagger = new MaxentTagger(modelFile);
+		@SuppressWarnings("unchecked")
+		String testFile = "bin/config/sampleinput.txt";
+		List<List<HasWord>> sentences = tagger.tokenizeText(new BufferedReader(new FileReader(testFile)));
+		for (List<HasWord> sentence : sentences) {
+			ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
+			System.out.println(Sentence.listToString(tSentence, false));
+		}
+	}
+  
+	public void getTagging(String content, String model){
+		try {
+			MaxentTagger tagger = new MaxentTagger(modelFile);
+			String tagged = tagger.tagTokenizedString(content);
+			System.out.println(tagged);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	  
+	}
 
 }
 
