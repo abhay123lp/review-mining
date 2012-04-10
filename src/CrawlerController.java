7c0e80e2d87cab7c381ddfa8f58277fdb9c6b50a
shaoxinjiang <shaoxinjiang@gmailcom>
2012-04-10 00:01:20 -0500

    Added Web Crawler and get the google search count number

--- src/CrawlerController.java
+++ src/CrawlerController.java
@@ -1,4 +1,101 @@
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
 
 public class CrawlerController {
+	static CrawlController controller;
+	int numberOfCrawlers = 3;
+    public void setup(){
 
+        /*
+         * crawlStorageFolder is a folder where intermediate crawl data is
+         * stored.
+         */
+        String crawlStorageFolder = "./";
+
+        /*
+         * numberOfCrawlers shows the number of concurrent threads that should
+         * be initiated for crawling.
+         */
+        int numberOfCrawlers = Integer.parseInt("3");
+
+        CrawlConfig config = new CrawlConfig();
+
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Be polite: Make sure that we don't send more than 1 request per
+         * second (1000 milliseconds between requests).
+         */
+        config.setPolitenessDelay(1000);
+
+        /*
+         * You can set the maximum crawl depth here. The default value is -1 for
+         * unlimited depth
+         */
+        config.setMaxDepthOfCrawling(0);
+
+        /*
+         * You can set the maximum number of pages to crawl. The default value
+         * is -1 for unlimited number of pages
+         */
+        config.setMaxPagesToFetch(1000);
+
+        /*
+         * Do you need to set a proxy? If so, you can use:
+         * config.setProxyHost("proxyserver.example.com");
+         * config.setProxyPort(8080);
+         * 
+         * If your proxy also needs authentication:
+         * config.setProxyUsername(username); config.getProxyPassword(password);
+         */
+
+        /*
+         * This config parameter can be used to set your crawl to be resumable
+         * (meaning that you can resume the crawl from a previously
+         * interrupted/crashed crawl). Note: if you enable resuming feature and
+         * want to start a fresh crawl, you need to delete the contents of
+         * rootFolder manually.
+         */
+        config.setResumableCrawling(false);
+        
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        try {
+			controller = new CrawlController(config, pageFetcher, robotstxtServer);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+
+        controller.addSeed("http://www.google.com/search?hl=en&q=ipod&aq=f&oq=&aqi=g10");
+        //controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        //controller.addSeed("http://www.ics.uci.edu/~welling/");
+
+      
+        
+
+    }
+    
+    public void run() throws Exception{
+    	 /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+    	controller.start(Crawler.class, numberOfCrawlers);
+    }
 }
+
--- src/main.java
+++ src/main.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+
 
 /**
  * 
@@ -20,20 +34,27 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//DBConnector db = new DBConnector();
-		//db.sql("select * from reviews_review limit 2");
-		
-		//DAO dao = new DAO();
-		//ArrayList<Review> reviews = dao.getReviews(404, 0, 100, 1);
-		//testTagging();
+		//System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.Jdk14Logger");
+		//testCrawler();
+		try {
+			testSearch();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*Document doc = null;
+		try {
+			doc = Jsoup.connect("http://www.google.com/search?hl=en&q=ipod&aq=f&oq=&aqi=g10").userAgent("Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.6) Gecko/20070723 Iceweasel/2.0.0.6 (Debian-2.0.0.6-0etch1)").get();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Elements newsHeadlines = doc.select("resultStats");
+		System.out.println(newsHeadlines.toString());*/
 		
 		DAO dao = new DAO();
-		//ArrayList<Review> reviews = dao.getReviewsByStoreId(404, 0, 100, 1);
-		//System.out.println(dao.getStoreIds().size());
-		//System.out.println(dao.getReviewerIds().size());
-		//System.out.println(dao.getReviewerIds(40, 70));
 		testTwoWordPhrase();
+
 	}
 	public static void testTagging(){
 		// test Tagging 
@@ -50,28 +71,29 @@ public class main {
 				);
 
 	}
+
 	
 	public static void testTwoWordPhrase() {
 		DAO dao = new DAO();
 		int count = 0;
 		//Timestamp t = 
 		try {
-			FileWrite fw = new FileWrite("test_output.txt");
+			FileWriter fw = new FileWriter("test_output.txt");
 			//Tagging t = new Tagging();
 			SO so = new SO();
 			
 			ArrayList<Review> reviews = dao.getReviewsByStoreId(404, 0, 100, 1);
 			for (Review review: reviews) {
 				ArrayList<String> phrases = so.getTwoWordPhrases(review.text);
-				fw.writeLine(review.text + "\n" + phrases.toString());
+				fw.write(review.text + "\n" + phrases.toString());
 				int i=0, k, sum=0;
 				for (String s: phrases) {
 					k = so.getPolarityOfPhrase(s);
-					fw.writeLine(s + " : " + k );
+					fw.write(s + " : " + k );
 					sum += k;
 					i++;
 				}
-				fw.writeLine("rating: "+ review.rating + " " + 
+				fw.write("rating: "+ review.rating + " " + 
 							(i > 0 ?(1.0*sum/i):0));
 				System.out.println("... processed " + ++count + " ...");
 				if (count > 50) break; // more causes memory overflow
@@ -84,4 +106,43 @@ public class main {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public static long testSearch() throws IOException{
+		
+            try {
+                    URL url = new URL("http://www.google.com/search?hl=en&q=ipod&aq=f&oq=&aqi=g10");
+                    URLConnection conn =  url.openConnection();
+                    conn.setRequestProperty("User-Agent",
+                                    "Mozilla/5.0 (X11; U; Linux x86_64; en-GB; rv:1.8.1.6) Gecko/20070723 Iceweasel/2.0.0.6 (Debian-2.0.0.6-0etch1)");
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream())
+                    );
+                    String str;
+                    String content ="";
+                    while ((str = in.readLine()) != null) {
+                    	content += str;
+                		//System.out.println(newsHeadlines.toString());
+                		//System.out.println(str);
+                    }
+
+                    in.close();
+                    
+                    Document doc = null;
+                	doc =  Jsoup.parse(content);
+                	//doc.body().toString();
+                	//System.out.println(Jsoup.clean(str, myWhitelist));
+                	
+                	//System.out.println(doc.body().select("table tr td div#subform_ctrl").toString());
+                	Elements newsHeadlines = doc.body().select("table tr td div#subform_ctrl");
+                	String cnt = newsHeadlines.select("div > div:eq(1)").text().toString();
+                	String delims = "[ ]";
+                	String[] tokens = cnt.split(delims);
+                	return Long.parseLong(tokens[1].replace(",", ""));
+            }
+            catch (MalformedURLException e) {}
+            catch (IOException e) {}
+            return 0;
+	}
+
 }
